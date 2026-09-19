98268c8ce8ac3b36f88fafa324ee1a53dab4c6bc
kubuntugamer <chasbigham@gmail.com>
2026-09-19 17:07:19 -0500

    Build Update: Transition repository to a fully isolated local freestanding build target

--- FabricZC/src/kernel/fabriczc_mod.mod.c
+++
@@ -1,6 +0,0 @@
-#include <linux/module.h>
-#include <linux/vermagic.h>
-#include <linux/compiler.h>
-
-MODULE_INFO(vermagic, "7.2.6-1-liquorix-amd64 SMP preempt mod_unload");
-MODULE_INFO(name, "fabriczc_mod");
