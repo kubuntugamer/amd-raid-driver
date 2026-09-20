d49325dcc57216c470ee5a1b17a22567bbac46cd
kubuntugamer <chasbigham@gmail.com>
2026-09-19 23:48:29 -0500

    Production Final: Explicitly size bdev_handle arrays and initialize local path buffers to pass modern Kbuild loops

--- FabricZC/src/kernel/main.c
+++ FabricZC/src/kernel/main.c
@@ -116,7 +116,7 @@ static int __init fabriczc_init(void)
     struct queue_limits limits;
     sector_t total_array_sectors;
     int ret, i;
-    char path[32];
+    char path[64];
 
     printk(KERN_INFO "FabricZC Standalone: Universal 4-Disk RAID 0 Engine + Dynamic Geometry Mapping Initializing.\n");
 
